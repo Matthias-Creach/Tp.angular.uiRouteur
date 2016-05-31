68e834ea52be87539a04eb1159327aac26595897
root <root@vps270810.ovh.net>
2016-05-31 10:11:13 +0200

    Tp qui fonctionne !

--- js/index.js
+++ js/index.js
@@ -9,35 +9,69 @@ angular.module('cyrcleoflifeApp', ['ui.router', 'restangular'])
     $stateProvider
       .state('birth', {
         url: '/birth',
-        template: '<div>{{apiText.message}} {{apiText.message}} You are borned<br><button ui-sref=".childhood">childhood</button><div ui-view></div></div>',
-        controller: function($scope, apiText){
-          console.log(apiText);
-          $scope.apiText.message = apiText.message;
-          $scope.apiText.from = apiText.from;
-        },
+        template: '<div>{{apiText}}><button ui-sref=".childhood">Enfance</button><div ui-view></div></div>',
+	controller: function($scope, apiText){
+          $scope.apiText = apiText.message;
+	},
         resolve: {
            apiText : function(Restangular){
              Restangular.setBaseUrl('http://foaas.com/');
-             return Restangular.one('retard', 'moi').get();
-             console.log("Fin de la promesse");
+             return Restangular.one('awesome', 'moi').get();
            }
         }
+        
       })
       .state('birth.childhood', {
         url: '/childhood',
-        template: '<div>You are grown up<br><button ui-sref=".study">study</button><br><button ui-sref="birth.childhood.career">career</button><div ui-view></div></div>'
+        template: '<div>{{apiText}}<br><button ui-sref=".study">Etude</button><br><button ui-sref="birth.childhood.career">Carrière</button><div ui-view></div></div>',
+	controller: function($scope, apiText){
+          $scope.apiText = apiText.message;
+	},
+        resolve: {
+           apiText : function(Restangular){
+             Restangular.setBaseUrl('http://foaas.com/');
+             return Restangular.one('zain', 'moi').get();
+           }
+        }
       })
       .state('birth.childhood.study', {
         url: '/study',
-        template: '<div>Really ?<br><button ui-sref=".phd">phd</button><button ui-sref=".workinglife">workinglife</button><div ui-view></div></div>'
+        template: '<div>{{apiText}}<br><button ui-sref=".phd">phd</button><button ui-sref=".workinglife">Travaille le reste de ta vie</button><div ui-view></div></div>',
+	controller: function($scope, apiText){
+          $scope.apiText = apiText.message;
+	},
+        resolve: {
+           apiText : function(Restangular){
+             Restangular.setBaseUrl('http://foaas.com/');
+             return Restangular.one('what', 'moi').get();
+           }
+        }
       })
       .state('birth.childhood.study.phd', {
         url: '/phd',
-        template: '<div>Great, What next ?<br><button ui-sref=".workinglife">workinglife</button><div ui-view></div></div>'
+        template: '<div>{{apiText}}<br><button ui-sref=".workinglife">Travaille le reste de ta vie</button><div ui-view></div></div>',
+	controller: function($scope, apiText){
+          $scope.apiText = apiText.message;
+	},
+        resolve: {
+           apiText : function(Restangular){
+             Restangular.setBaseUrl('http://foaas.com/');
+             return Restangular.one('donut', 'moi').get();
+           }
+        }
       })
       .state('birth.childhood.career', {
         url: '/career',
-        template: '<div>GoodLuck !<br><button ui-sref=".workinglife">workinglife</button><div ui-view></div></div>'
+        template: '<div>{{apiText}}<br><button ui-sref=".workinglife">Travaille le reste de ta vie</button><div ui-view></div></div>',
+	controller: function($scope, apiText){
+          $scope.apiText = apiText.message;
+	},
+        resolve: {
+           apiText : function(Restangular){
+             Restangular.setBaseUrl('http://foaas.com/');
+             return Restangular.one('thanks', 'moi').get();
+           }
+        }
       });
 
       addWorkingLife($stateProvider, 'birth.childhood.study.phd');
@@ -47,15 +81,42 @@ angular.module('cyrcleoflifeApp', ['ui.router', 'restangular'])
       function addWorkingLife($stateProvider, parent){
         $stateProvider.state(`${parent}.workinglife`, {
           url: '/workinglife',
-          template: '<div>Hard Work !<br><button ui-sref=".pension">pension</button><div ui-view></div></div>'
+          template: '<div>{{apiText}}<br><button ui-sref=".pension">pension</button><div ui-view></div></div>',
+	  controller: function($scope, apiText){
+         	 $scope.apiText = apiText.message;
+	  },
+	  resolve: {
+	     apiText : function(Restangular){
+		     Restangular.setBaseUrl('http://foaas.com/');
+		     return Restangular.one('life', 'moi').get();
+	     }
+	  }
         })
         .state(`${parent}.workinglife.pension`, {
           url: '/pension',
-          template: '<div>Good old days<br><button ui-sref=".death">death</button><div ui-view></div></div>'
+          template: '<div>{{apiText}}<br><button ui-sref=".death">La mort</button><div ui-view></div></div>',
+	  controller: function($scope, apiText){
+         	 $scope.apiText = apiText.message;
+	  },
+	  resolve: {
+	     apiText : function(Restangular){
+	     Restangular.setBaseUrl('http://foaas.com/');
+	     return Restangular.one('because', 'moi').get();
+	    }
+	  }
         })
         .state(`${parent}.workinglife.pension.death`, {
           url: '/death',
-          template: '<div>RIP<br><button ui-sref="birth">birth</button></div>'
+          template: '<div>{{apiText}}<br><button ui-sref="birth">Renaissance !</button></div>',
+	  controller: function($scope, apiText){
+         	 $scope.apiText = apiText.message;
+	  },
+	  resolve: {
+	    apiText : function(Restangular){
+	     Restangular.setBaseUrl('http://foaas.com/');
+	     return Restangular.one('bye', 'moi').get();
+	    }
+	   }
         });
       }
 
